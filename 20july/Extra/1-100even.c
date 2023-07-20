4b521e814b2f1c36771101099614017fbbba4645
Gaurang Mishra <Gaurangmishra1008@gmail.com>
2023-07-20 14:46:36 +0530

    extra

---
+++ 20july/Extra/1-100even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num=1;
+    while(num<101)
+    {
+        if(num%2==0)
+			printf("%d ",num);
+
+		// increasing loop counter by 1
+		num++;
+    }return 0;
+    }
+
+
